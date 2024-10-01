5d31faf7f7868d61bf76b1f18932580be5cc8026
Abdelrahman-Adel610 <engba80818233@gmail.com>
2024-10-01 18:33:01 +0300

    ↗️ Enhanced stars component

--- src/Components/Stars.js
+++ src/Components/Stars.js
@@ -1,44 +1,19 @@
 import { useState } from "react";
 
-const startStyle = {
-  cursor: "Pointer",
-  width: "30px",
-  height: "30px",
-};
-const fullStart = (
-  <svg
-    style={startStyle}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="#000"
-    stroke="#000"
-  >
-    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-  </svg>
-);
-const emptyStar = (
-  <svg
-    style={startStyle}
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="#000"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="{2}"
-      d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-    />
-  </svg>
-);
 const startsContainerStyle = {
   display: "flex",
   alignItems: "center",
   gap: "10px",
 };
-export default function Stars({ num = 10 }) {
-  const [rate, setRate] = useState(0);
+export default function Stars({
+  num = 10,
+  size = 48,
+  color = "#fcc419",
+  defaultRating = 3,
+  setRateOut = undefined,
+  messages = [],
+}) {
+  const [rate, setRate] = useState(defaultRating);
   const [hoverRate, setHoverRate] = useState(0);
   return (
     <div style={startsContainerStyle}>
@@ -46,24 +21,57 @@ export default function Stars({ num = 10 }) {
         <Star
           key={i}
           full={hoverRate ? i + 1 <= hoverRate : i + 1 <= rate}
-          onClickHandler={() => setRate(i + 1)}
+          onClickHandler={() => {
+            setRate(i + 1);
+            setRateOut && setRateOut(i + 1);
+          }}
           onEnter={() => setHoverRate(i + 1)}
           onLeave={() => setHoverRate(0)}
+          size={size}
+          color={color}
         />
       ))}
-      <p>{hoverRate || rate || ""}</p>
+      <p style={{ color: color, fontSize: size / 1.5 }}>
+        {messages.length === num
+          ? messages[(hoverRate || rate )-1]
+          : hoverRate || rate || ""}
+      </p>
     </div>
   );
 }
-function Star({ full, onClickHandler, onEnter, onLeave }) {
+function Star({ full, onClickHandler, onEnter, onLeave, size, color }) {
   return (
     <span
+      style={{ width: `${size}px`, cursor: "pointer", height: `${size}px` }}
       role="button"
       onClick={onClickHandler}
       onMouseEnter={onEnter}
       onMouseLeave={onLeave}
     >
-      {full ? fullStart : emptyStar}
+      {full ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill={color}
+          stroke={color}
+        >
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke={color}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="{2}"
+            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+          />
+        </svg>
+      )}
     </span>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./Components/App";
 import Stars from "./Components/Stars";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    {/* <Stars num={5} /> */}
+    {/* <App /> */}
+    <Stars
+      num={5}
+      messages={["bad", "good", "med", "very good", "excellent"]}
+      color="blue"
+      size={32}
+    />
   </React.StrictMode>
 );
