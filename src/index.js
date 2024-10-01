8e4b8a267b27cd9d831e3e20c074bcb12dc65ffb
Abdelrahman-Adel610 <engba80818233@gmail.com>
2024-10-01 17:42:02 +0300

    ⭐ Added stars reusable component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./Components/App";
+import Stars from "./Components/Stars";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Stars num={5} /> */}
   </React.StrictMode>
 );
